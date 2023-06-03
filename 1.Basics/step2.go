027d8cdfa46cfbc32ad399a3a3baa5870e5d57f5
Abdulazizkhann <abdulaziznurullayv@gmail.com>
2023-06-03 17:41:12 +0500

    my code

---
+++ 1.Basics/step2.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+// println("Hello Golang !")
+// println("Hello Ali")
+// println("Hello World")
+
+// VARIBLE ---> o'zgaruvchi
+//VAR      ---> keyvord, kalit so'z
+
+// declare        = assignmend
+// tanitish       = tenglashtirish, tayinlash
+var name string = "Hello Golang"
+println(name)
+
+/*   boshqa sahifadan o;zgaruvchi kerak bo'lsa katta hrfda yoziladi !!!
+     kichik harfda faqat o'z sahifamizda foydalanamiz !!!
+*/
+     var Name string = "Go programming language"
+	 println(Name)
+
+	 var myName string = "Alixan" // CamelClas --> yozish etikasi
+	 println(Name)
+
+	 var _a string = "A"
+	 println(_a)
+
+	 var b_ string = "A"
+	 println(b_)
+}
