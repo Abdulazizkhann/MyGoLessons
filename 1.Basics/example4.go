9af35451df730c1275a9151eced3cf6a57e2c761
Abdulazizkhann <abdulaziznurullayv@gmail.com>
2023-06-10 17:13:21 +0500

    golang basics  examples

--- 1.Basics/example4.go
+++ 1.Basics/example4.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-
-	//BO'SH TANIVCHI
+	
+	// BO'SH TANITUVCHI
 	//db_get() --> data, err
-	//_, data :=get_data()
+	//_, data := get_data()
 	//fmt.Println(data)
 
 	var _ string = "Hello"
-	// fmt.Println(_)
+	//fmt.Println(_)
 
 	var _, x, _ = 0, 10, 0
 	fmt.Println(x)
